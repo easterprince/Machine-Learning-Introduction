ef030d69bf14604b017ce7053a67e52cfe877b4f
Sevastian Kozhunov <sky.seva@gmail.com>
2020-03-26 18:14:53 +0200

    Find weird test for decision tree fitting

--- materials/homeworks/2_decision_tree_knn/MyDecisionTree.py
+++ materials/homeworks/2_decision_tree_knn/MyDecisionTree.py
@@ -3,14 +3,14 @@ from sklearn.base import BaseEstimator
 from collections import Counter
 import heapq
 
+
 class DecisionTree(BaseEstimator):
-    
     class NotFittedError(Exception):
         pass
-    
+
     class UnknownCriterionError(Exception):
         pass
-    
+
     class TreeNode:
         def __init__(self, tree, measure, predict, X, y, depth = 0, class_count = 0):
             self.__tree = tree
@@ -24,38 +24,41 @@ class DecisionTree(BaseEstimator):
             if depth >= tree.max_depth or X.shape[0] < tree.min_samples_split:
                 self.__predicted = predict(y)
                 return
-            
+
+            best_quality = None
             for feature in range(X.shape[1]):
                 rearrange = np.argsort(X[:, feature])
                 sorted_features = X[rearrange, feature]
                 sorted_y = y[rearrange]
-                left_criterions = measure(sorted_y)
-                right_criterions = np.flip(measure(np.flip(sorted_y)))
+                left_criteria = measure(sorted_y)
+                right_criteria = np.flip(measure(np.flip(sorted_y)))
                 for separator in range(1, sorted_features.shape[0]):
                     if sorted_features[separator] == sorted_features[separator - 1]:
                         continue
-                    quality = left_criterions[-1]
-                    quality -= separator / sorted_features.shape[0] * left_criterions[separator - 1]
-                    quality -= (1 - separator / sorted_features.shape[0]) * right_criterions[separator]
-                    if self.__feature == None or quality > best_quality:
+                    quality = left_criteria[-1]
+                    quality -= separator / sorted_features.shape[0] * left_criteria[separator - 1]
+                    quality -= (1 - separator / sorted_features.shape[0]) * right_criteria[separator]
+                    if self.__feature is None or quality > best_quality:
                         best_quality = quality
                         self.__feature = feature
                         self.__threshold = (sorted_features[separator] + sorted_features[separator - 1]) / 2
-            
-            if self.__feature != None:
+
+            if self.__feature is not None:
                 go_left = (X[:, self.__feature] < self.__threshold)
-                self.__left_child = tree.TreeNode(tree, measure, predict, X[go_left], y[go_left], depth + 1, class_count)
-                self.__right_child = tree.TreeNode(tree, measure, predict, X[~go_left], y[~go_left], depth + 1, class_count)
+                self.__left_child = tree.TreeNode(tree, measure, predict, X[go_left], y[go_left], depth + 1,
+                                                  class_count)
+                self.__right_child = tree.TreeNode(tree, measure, predict, X[~go_left], y[~go_left], depth + 1,
+                                                   class_count)
             else:
                 self.__predicted = predict(y)
-        
+
         def predict(self, X, probability = False):
             if not probability:
                 predicted = np.zeros(shape = (X.shape[0]))
             else:
                 predicted = np.zeros(shape = (X.shape[0], self.__class_count))
             if X.shape[0] != 0:
-                if self.__feature == None:
+                if self.__feature is None:
                     if not probability:
                         predicted[:] = self.__predicted[0]
                     else:
@@ -66,8 +69,7 @@ class DecisionTree(BaseEstimator):
                     predicted[go_left] = self.__left_child.predict(X[go_left], probability = probability)
                     predicted[~go_left] = self.__right_child.predict(X[~go_left], probability = probability)
             return predicted
-        
-    
+
     def __init__(self, max_depth = np.inf, min_samples_split = 2,
                  criterion = 'gini', debug = False):
         self.max_depth = max_depth
@@ -75,24 +77,26 @@ class DecisionTree(BaseEstimator):
         self.criterion = criterion
         self.__debug = debug
         self.__root = None
-    
+        self.__class_count = None
+        self.median_dif_ = 0
+
     def fit(self, X, y):
         if X.shape[0] != y.shape[0]:
             raise ValueError('Numbers of objects in X and y are not equal.')
         measure, predict = self.__get_methods(self.criterion)
         self.__class_count = int(np.max(y) + 1)
         self.__root = self.TreeNode(self, measure, predict, X, y, class_count = self.__class_count)
-            
+
     def predict(self, X, probability = False):
-        if self.__root == None:
+        if self.__root is None:
             raise self.NotFittedError
         return self.__root.predict(X, probability = probability)
-    
+
     def predict_proba(self, X):
         return self.predict(X, probability = True)
-    
+
     def __get_methods(self, criterion):
-        
+
         def classic_entropy(y):
             partials = np.zeros(shape = y.shape[0])
             counter = {}
@@ -101,7 +105,7 @@ class DecisionTree(BaseEstimator):
                 p = np.array(list(counter.values())) / (i + 1)
                 partials[i] = -np.sum(p * np.log2(p))
             return partials
-        
+
         def entropy(y):
             partials = np.zeros(shape = y.shape[0])
             logs = self.__logs
@@ -118,7 +122,7 @@ class DecisionTree(BaseEstimator):
                 counter[y_i] = count_y_i
                 partials[i] = (logs[i + 1] * (i + 1) - cur_log_sum) / (i + 1)
             return partials
-        
+
         def classic_gini(y):
             partials = np.zeros(shape = y.shape[0])
             counter = {}
@@ -127,7 +131,7 @@ class DecisionTree(BaseEstimator):
                 p = np.array(list(counter.values())) / (i + 1)
                 partials[i] = 1 - np.sum(p * p)
             return partials
-        
+
         def gini(y):
             partials = np.zeros(shape = y.shape[0])
             counter = {}
@@ -135,20 +139,20 @@ class DecisionTree(BaseEstimator):
             for i, y_i in enumerate(y):
                 count_y_i = counter.get(y_i, 0)
                 if count_y_i > 0:
-                    cur_sum_squared -= count_y_i * count_y_i 
+                    cur_sum_squared -= count_y_i * count_y_i
                 count_y_i += 1
                 cur_sum_squared += count_y_i * count_y_i
                 counter[y_i] = count_y_i
                 partials[i] = 1 - cur_sum_squared / ((i + 1) * (i + 1))
             return partials
-        
+
         def classic_variance(y):
             partials = np.zeros(shape = y.shape[0])
             for i in range(y.shape[0]):
                 prefix = y[:(i + 1)]
                 partials[i] = np.mean((prefix - np.mean(prefix)) ** 2)
             return partials
-        
+
         def variance(y):
             partials = np.zeros(shape = y.shape[0])
             cur_sum = 0.0
@@ -158,44 +162,16 @@ class DecisionTree(BaseEstimator):
                 cur_square_sum += y_i ** 2
                 partials[i] = cur_square_sum / (i + 1) - (cur_sum / (i + 1)) ** 2
             return partials
-        
+
         def classic_mad_median(y):
             partials = np.zeros(shape = y.shape[0])
             for i in range(y.shape[0]):
                 prefix = y[:(i + 1)]
                 partials[i] = np.mean(np.abs(prefix - np.median(prefix)))
+            dif = np.max(np.abs(partials - mad_median(y)))
+            self.median_dif_ = max(self.median_dif_, dif)
             return partials
-        
-        def classic_mad_median(y):
-            partials = np.zeros(shape = y.shape[0])
-            left_sum = 0.0
-            left_heap = []
-            right_sum = 0.0
-            right_heap = []
-            for i, num in enumerate(y):
-                if len(left_heap) == 0 or num < -left_heap[0]:
-                    left_sum += num
-                    heapq.heappush(left_heap, -num) 
-                else:
-                    right_sum += num
-                    heapq.heappush(right_heap, num)
-                if len(left_heap) < len(right_heap):
-                    num = heapq.heappop(right_heap)
-                    right_sum -= num
-                    left_sum += num
-                    heapq.heappush(left_heap, -num) 
-                elif len(left_heap) > len(right_heap) + 1:
-                    num = -heapq.heappop(left_heap)
-                    left_sum -= num
-                    right_sum += num
-                    heapq.heappush(right_heap, num)
-                if len(left_heap) == len(right_heap):
-                    median = (-left_heap[0] + right_heap[0]) / 2
-                else:
-                    median = -left_heap[0]
-                partials[i] = (right_sum - left_sum + (len(left_heap) - len(right_heap)) * median) / (i + 1)
-            return partials
-        
+
         def mad_median(y):
             partials = np.zeros(shape = y.shape[0])
             left_sum = 0.0
@@ -205,7 +181,7 @@ class DecisionTree(BaseEstimator):
             for i, num in enumerate(y):
                 if len(left_heap) == 0 or num < -left_heap[0]:
                     left_sum += num
-                    heapq.heappush(left_heap, -num) 
+                    heapq.heappush(left_heap, -num)
                 else:
                     right_sum += num
                     heapq.heappush(right_heap, num)
@@ -213,7 +189,7 @@ class DecisionTree(BaseEstimator):
                     num = heapq.heappop(right_heap)
                     right_sum -= num
                     left_sum += num
-                    heapq.heappush(left_heap, -num) 
+                    heapq.heappush(left_heap, -num)
                 elif len(left_heap) > len(right_heap) + 1:
                     num = -heapq.heappop(left_heap)
                     left_sum -= num
@@ -225,24 +201,25 @@ class DecisionTree(BaseEstimator):
                     median = -left_heap[0]
                 partials[i] = (right_sum - left_sum + (len(left_heap) - len(right_heap)) * median) / (i + 1)
             return partials
-        
+
         def classify(y):
             probabilities = Counter(y)
             result = None
+            biggest_count = None
             for tag, count in probabilities.items():
-                if result == None or count > biggest_count:
+                if result is None or count > biggest_count:
                     result = tag
                     biggest_count = count
                 probabilities[tag] = count / y.shape[0]
             return result, probabilities
-        
+
         def interpolate(y):
             return y.mean(), None
-        
+
         if criterion == 'entropy':
             self.__logs = [0]
-        
-        criterions = {
+
+        criteria = {
             'entropy': (entropy, classify),
             'gini': (gini, classify),
             'variance': (variance, interpolate),
@@ -252,7 +229,7 @@ class DecisionTree(BaseEstimator):
             'classic_variance': (classic_variance, interpolate),
             'classic_mad_median': (classic_mad_median, interpolate)
         }
-        if criterion not in criterions:
+        if criterion not in criteria:
             raise self.UnknownCriterionError(criterion)
-        measure, predict = criterions[criterion]
+        measure, predict = criteria[criterion]
         return measure, predict
